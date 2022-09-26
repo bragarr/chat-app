75868c065def6825c5b4f970b792bf07f0f311ab
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-26 09:37:05 -0400

    fix: Ajustado bug que usuário acessava página de login/signIn logado

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,12 +14,18 @@ const Private = ({Item}) => {
     return logado > 0 ? <Item /> : <Login /> 
 }
 
+const PrivateLoginSign = ({Item}) => {
+    let { logado } = useAuth();
+
+    return logado > 0 ? <User />  : <Item />
+}
+
 export function MainRoutes() {
     return(
         <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="signin" element={<SignIn />} />
+            <Route path="login" element={<PrivateLoginSign Item={Login} />} />
+            <Route path="signin" element={<PrivateLoginSign Item={SignIn} />} />
             <Route path="chatroom" element={<Private Item={ChatRoom} />} />
             <Route path="user" element={<User />} />
         </Routes>
